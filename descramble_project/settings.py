4050bc5ee8769f804c2311a0551d4edbddbf62aa
Kimari <grgkimari@gmail.com>
2022-06-28 16:53:41 +0300

    partially fixed background image

--- descramble_project/settings.py
+++ descramble_project/settings.py
@@ -125,19 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version' : 1,
-    'disable_existing_loggers' : False,
-    'loggers' : {
-        'logger1' : {
-            'level' : os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-            'propagate' : False,
-        },
-    },
-}
